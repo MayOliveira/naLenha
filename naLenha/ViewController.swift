c9017c0beebc9a804ebcd25e9e3d16618ff764d8
Mayara Ferreira de Oliveira <mmayolliveira@gmail.com>
2018-08-02 14:45:38 -0400

    Initial Commit

---
+++ naLenha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  naLenha
+//
+//  Created by Mayara Ferreira de Oliveira on 02/08/2018.
+//  Copyright © 2018 Mayara Ferreira de Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
